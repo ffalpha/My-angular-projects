7ae8c4433df6d459628cf7022bb0307bb730201a
ffalpha <kalanam217@gmail.com>
2019-09-01 00:38:39 +0530

    created regsitration module

--- realworldapp/src/app/core/core.module.ts
+++ realworldapp/src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AuthModule } from '../auth/auth.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AuthModule
   ]
 })
 export class CoreModule { }
